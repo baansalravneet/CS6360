098d7ef4cda5faa8490e45047c48b370fe4349f8
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2023-09-23 12:20:24 -0500

    added database config

---
+++ src/main/java/com/librarysystem/LibrarySystemApplication.java
@@ -0,0 +1,16 @@
+package com.librarysystem;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class LibrarySystemApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(LibrarySystemApplication.class)
+                .headless(false)
+                .web(WebApplicationType.NONE)
+                .run(args);
+    }
+}
--- src/main/java/com/librarysystem/db/Database.java
+++ src/main/java/com/librarysystem/db/Database.java
@@ -1,8 +1,13 @@
 package com.librarysystem.db;
 
+import com.librarysystem.config.DatabaseConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class Database {
+    @Autowired
+    public Database(DatabaseConfig config) {
 
+    }
 }
