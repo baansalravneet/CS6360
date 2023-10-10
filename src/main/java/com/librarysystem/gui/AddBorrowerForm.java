560d2e0cf22e1232281cdee424d7570aaad5c1ad
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2023-10-10 15:29:59 -0500

    added exit button in borrower form

--- src/main/java/com/librarysystem/gui/AddBorrowerForm.java
+++ src/main/java/com/librarysystem/gui/AddBorrowerForm.java
@@ -16,9 +16,10 @@ public class AddBorrowerForm extends JFrame {
 
     private void configure() {
         this.setTitle("Registration Form");
-        this.setBounds(0, 0, 900, 600);
+        this.setBounds(0, 0, 350, 400);
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.setResizable(false);
+        MainWindow.centerFrameOnScreen(this);
 
         Container content = this.getContentPane();
         content.setLayout(null);
@@ -28,70 +29,68 @@ public class AddBorrowerForm extends JFrame {
         content.add(ssn);
 
         JTextField ssnInput = new JTextField();
-        ssnInput.setBounds(ssn.getX() + 100, ssn.getY(), 190, 20);
+        ssnInput.setBounds(130, 30, 190, 20);
         content.add(ssnInput);
 
         JLabel firstName = new JLabel("First Name");
-        firstName.setBounds(ssn.getX(), ssn.getY() + 25, 100, 20);
+        firstName.setBounds(30, 55, 100, 20);
         content.add(firstName);
 
         JTextField firstNameInput = new JTextField();
-        firstNameInput.setBounds(firstName.getX() + 100, firstName.getY(), 190, 20);
+        firstNameInput.setBounds(130, 55, 190, 20);
         content.add(firstNameInput);
 
         JLabel lastName = new JLabel("Last Name");
-        lastName.setBounds(firstName.getX(), firstName.getY() + 25, 100, 20);
+        lastName.setBounds(30, 80, 100, 20);
         content.add(lastName);
 
         JTextField lastNameInput = new JTextField();
-        lastNameInput.setBounds(lastName.getX() + 100, lastName.getY(), 190, 20);
+        lastNameInput.setBounds(30 + 100, 80, 190, 20);
         content.add(lastNameInput);
 
         JLabel email = new JLabel("Email");
-        email.setBounds(lastName.getX(), lastName.getY() + 25, 100, 20);
+        email.setBounds(30, 105, 100, 20);
         content.add(email);
 
         JTextField emailInput = new JTextField();
-        emailInput.setBounds(email.getX() + 100, email.getY(), 190, 20);
+        emailInput.setBounds(30 + 100, 105, 190, 20);
         content.add(emailInput);
 
         JLabel address = new JLabel("Address");
-        address.setBounds(email.getX(), email.getY() + 25, 100, 26);
+        address.setBounds(30, 130, 100, 26);
         content.add(address);
 
         JTextArea addressInput = new JTextArea();
-        addressInput.setBounds(address.getX() + 105, address.getY(), 180, 60);
+        addressInput.setBounds(135, 130, 180, 60);
         addressInput.setLineWrap(true);
         content.add(addressInput);
 
         JLabel city = new JLabel("City");
-        city.setBounds(address.getX(), address.getY() + 25 + 40, 100, 20);
+        city.setBounds(30, 195, 100, 20);
         content.add(city);
 
         JTextField cityInput = new JTextField();
-        cityInput.setBounds(city.getX() + 100, city.getY(), 190, 20);
+        cityInput.setBounds(130, 195, 190, 20);
         content.add(cityInput);
 
         JLabel state = new JLabel("State");
-        state.setBounds(city.getX(), city.getY() + 25, 100, 20);
+        state.setBounds(30, 220, 100, 20);
         content.add(state);
 
         JTextField stateInput = new JTextField();
-        stateInput.setBounds(state.getX() + 100, state.getY(), 190, 20);
+        stateInput.setBounds(130, 220, 190, 20);
         content.add(stateInput);
 
         JLabel phone = new JLabel("Phone");
-        phone.setBounds(state.getX(), state.getY() + 25, 100, 20);
+        phone.setBounds(30, 245, 100, 20);
         content.add(phone);
 
         JTextField phoneInput = new JTextField();
-        phoneInput.setBounds(phone.getX() + 100, phone.getY(), 190, 20);
+        phoneInput.setBounds(130, 245, 190, 20);
         content.add(phoneInput);
 
         JButton addButton = new JButton("Register");
-        addButton.setBounds(phone.getX() + 40, phone.getY() + 50, 100, 20);
-        content.add(addButton);
-
+        addButton.setBounds(70, 295, 100, 20);
         addButton.addActionListener(listener -> {
             String _ssn = ssnInput.getText();
             String _firstName = firstNameInput.getText();
@@ -109,20 +108,20 @@ public class AddBorrowerForm extends JFrame {
                     || _address.length() == 0
                     || _city.length() == 0
                     || _state.length() == 0
-                    || _phone.length() == 0) MainWindow.showErrorFrame();
+                    || _phone.length() == 0) {
+                MainWindow.showErrorFrame();
+            } else {
                 // TODO: make this into a request object
                 // TODO: return a proper error message
-            else {
                 if (databaseService.registerBorrower(_ssn, _firstName, _lastName,
                         _email, _address, _city, _state, _phone)) MainWindow.showSuccessFrame();
                 else MainWindow.showErrorFrame();
             }
         });
+        content.add(addButton);
 
         JButton resetButton = new JButton("Reset");
-        resetButton.setBounds(addButton.getX() + 110, addButton.getY(), 100, 20);
-        content.add(resetButton);
-
+        resetButton.setBounds(180, 295, 100, 20);
         resetButton.addActionListener(listener -> {
             ssnInput.setText("");
             firstNameInput.setText("");
@@ -133,6 +132,14 @@ public class AddBorrowerForm extends JFrame {
             stateInput.setText("");
             phoneInput.setText("");
         });
+        content.add(resetButton);
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.setBounds(125, 325, 100, 20);
+        exitButton.addActionListener(listener -> {
+            this.dispose();
+        });
+        content.add(exitButton);
 
         this.setVisible(true);
     }
